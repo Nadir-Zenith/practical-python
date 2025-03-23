ba57af187d019f51719fb634631169aa4ffb0e8e
Bijaya Khadka <zen.kalon@gmail.com>
2025-03-23 16:08:46 +0545

    boucing balls

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100 #meters
+bounce_back_ratio = 3.0/5.0
+
+for i in range(10):
+    height *= bounce_back_ratio
+    print(f'{i+1:<4} bounce, height: {round(height,4):>10} meters')
