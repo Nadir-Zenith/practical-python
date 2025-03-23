396b76dc0e067667013c1b0586ee71e151feebf9
Bijaya Khadka <zen.kalon@gmail.com>
2025-03-23 16:35:55 +0545

    extra payment for 12 months

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -5,11 +5,18 @@
 years = 30
 mortgage = 500_000
 interest_rate = 0.05
-monthly_payment = 2684.11
+payment = 2684.11
 total_sum = 0.0
+months = 0
 
 while mortgage > 0:
+    months += 1
+    monthly_payment = payment
+    if months <= 12:
+        monthly_payment += 1000
+        
     total_sum += monthly_payment
     mortgage = mortgage * (1 +  interest_rate/12) - monthly_payment
+    
 
-print(total_sum)
+print(f'{total_sum=} ,in {months=}' )
