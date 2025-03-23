6e3f59322a939027b42c6900373a80af346057c9
Bijaya Khadka <zen.kalon@gmail.com>
2025-03-23 19:07:31 +0545

    file i/o

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+
+path = "./Data/portfolio.csv"
+total_cost = 0
+with open(path, 'r') as f:
+    headers = next(f).split(',')
+    for line in f:
+        row = line.split(',')
+        total_cost += int(row[1]) * float(row[2])
+
+print(f'{total_cost=}')
